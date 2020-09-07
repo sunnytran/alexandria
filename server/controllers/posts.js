2346c7ef76a814481d35e8ea44b6aadc6edc0ed2
sunny <sunnydinhtran@gmail.com>
2020-09-07 15:37:44 -0500

    just need to update image server code

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,7 +4,30 @@ const handlePostsGet = (db) => async (req, res) => {
   res.json(posts);
 };
 
+const handlePostsPost = (db) => async (req, res) => {
+  // var img = fs.readFileSync(req.file.path);
+  // var encoded = img.toString("base64");
+  // var finalImg = {
+  //   contentType: req.file.mimetype,
+  //   image: Buffer.from(encoded, "base64"),
+  // };
+
+  data = {
+    username: "Anonymous",
+    date: new Date(),
+    comment: req.body.comment,
+    board: req.body.board,
+    // image: finalImg,
+  };
+
+  const post = await db("posts")
+    .insert(data)
+    .returning("*")
+    .then((res) => res);
+  res.json(post[0]);
+};
+
 module.exports = {
   handlePostsGet: handlePostsGet,
-  // hanldePostsPost: hanldeRepliesPost,
+  handlePostsPost: handlePostsPost,
 };
