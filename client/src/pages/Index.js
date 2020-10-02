bd130cbe8c4f35ffc6b11f8cd6f7c6ca7b1a94e7
sunny <sunnydinhtran@gmail.com>
2020-10-01 20:14:44 -0500

    not doing the search feature

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -8,7 +8,6 @@ import "../styles/main.css";
 
 import Header from "../components/Header";
 import ContentBorder from "../components/content/ContentBorder";
-import SearchContent from "../components/SearchContent";
 import Stats from "../components/Stats";
 
 function Index({ boards, getBoards }) {
@@ -28,18 +27,24 @@ function Index({ boards, getBoards }) {
 
         <div class="flex space-x-5">
           <div class="w-1/5 space-y-5">
-            <SearchContent />
-            <Stats />
-          </div>
-
-          <div class="w-4/5 space-y-5">
             <ContentBorder title="About" borderColor="orange-300">
               Alexandria is a simple imageboard clone where you can post
               comments and share images. There are boards dedicated to a variety
               of topics that you can participate in without needing to register
               for an account!
+              <br />
+              <a
+                class="no-underline text-blue-500 hover:underline hover:text-white"
+                href="https://github.com/sunnytran"
+                target="_blank"
+              >
+                My GitHub
+              </a>
             </ContentBorder>
+            <Stats />
+          </div>
 
+          <div class="w-4/5 space-y-5">
             <ContentBorder title="Boards">
               <div class="flex">
                 {categories.map((i) => {
@@ -66,20 +71,6 @@ function Index({ boards, getBoards }) {
                   );
                 })}
               </div>
-              {/* <ul>
-                {boards.map((i) => {
-                  return (
-                    <li key={i.id}>
-                      <Link
-                        class="no-underline text-blue-500 hover:underline hover:text-white"
-                        to={"/" + i.name}
-                      >
-                        {i.title}
-                      </Link>
-                    </li>
-                  );
-                })}
-              </ul> */}
             </ContentBorder>
 
             <ContentBorder title="Popular">Popular posts go here</ContentBorder>
